16325e74829ea511c3c6c24079588e1cc6aa0d83
0HA1 <111664341+0HA1@users.noreply.github.com>
2026-01-01 15:29:14 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+local Loader = {}
+
+function Loader:Init(Table)
+    local Scene = Table
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b2d46cd7260086316b77133afd5f0d0a.lua"))()
+end
+
+return Loader
